6d5c6635cfa271f8b9900d7538819c6c586885f1
andsbilly22 <60950200+andsbilly22@users.noreply.github.com>
2020-03-03 22:16:02 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,14 @@
+autores:1DSI3
+@Anderson And.s
+instituição Etec
+
+feito em 03/30/2020
+x = 0 //declaração do contador
+valor = 2 //declaração da variavel valor
+while (x <= 21):
+valor = valor * valor
+x = x + 1  //laço de repetição
+print (valor) //exibir a variavel valor
+
+
+
